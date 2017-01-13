578b46ba38f934c0ca61a1c0eca2d49f52495099
wandererZoe <zoetxt@outlook.com>
2017-01-13 14:28:07 +0800

    添加枚举类

---
+++ SunBoom/src/main/java/enums/ActivityType.java
@@ -0,0 +1,24 @@
+package enums;
+
+/**
+ * Created by zoetx on 2017/1/13.
+ */
+public enum ActivityType {
+
+    VOLUNTEER(0),
+    MUSIC(1),
+    SPORT(2);
+
+    private int index;
+    private ActivityType(int index){
+        this.index = index;
+    }
+
+    public int getIndex() {
+        return index;
+    }
+
+    public void setIndex(int index) {
+        this.index = index;
+    }
+}
---
+++ SunBoom/src/main/java/enums/ResultMsg.java
@@ -0,0 +1,25 @@
+package enums;
+
+/**
+ * Created by zoetx on 2017/1/13.
+ */
+public enum ResultMsg {
+    offline("断网");//断网
+
+    private String message;
+
+    private ResultMsg(String ms){
+        this.message = ms;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+}
+
+
