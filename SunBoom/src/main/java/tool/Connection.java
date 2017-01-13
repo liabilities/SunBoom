475c2b31fe02b599a5a6674ee773c8e3f545aea4
zs14 <1509403958@qq.com>
2017-01-13 17:59:44 +0800

    add

---
+++ SunBoom/src/main/java/tool/Connection.java
@@ -0,0 +1,33 @@
+package tool;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+/**
+ * Created by mm on 2016/7/11.
+ */
+public class Connection {
+    private static Configuration configuration;
+    private static SessionFactory sessionFactory;
+
+    private Connection(){
+
+    }
+
+
+    public static Session getSession(){
+
+        if (sessionFactory==null){
+            configuration=new Configuration().configure();
+            sessionFactory=configuration.buildSessionFactory();
+            return sessionFactory.openSession();
+        }else {
+            return sessionFactory.openSession();
+        }
+    }
+
+    public static void closeSession(Session session){
+        session.close();
+    }
+}
