d3981810046790c1ff5c4d27edb0dd37883c79d6
wandererZoe <zoetxt@outlook.com>
2017-01-16 20:26:42 +0800

    私信DAO

---
+++ SunBoom/src/main/java/dao/PrivateLetterDAO.java
@@ -0,0 +1,9 @@
+package dao;
+
+import pojo.PrivateLetter;
+
+/**
+ * Created by zoetx on 2017/1/16.
+ */
+public interface PrivateLetterDAO extends BaseDAO<PrivateLetter>{
+}
---
+++ SunBoom/src/main/java/dao/impl/PrivateLetterDAOImpl.java
@@ -0,0 +1,12 @@
+package dao.impl;
+
+import dao.PrivateLetterDAO;
+import pojo.PrivateLetter;
+
+/**
+ * Created by zoetx on 2017/1/16.
+ */
+public class PrivateLetterDAOImpl extends BaseDAOImpl<PrivateLetter> implements PrivateLetterDAO{
+
+
+}
