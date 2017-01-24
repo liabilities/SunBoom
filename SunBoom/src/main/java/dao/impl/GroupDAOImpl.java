146eedc1cda65abe53b0298fdbd69b5107677e7d
wandererZoe <zoetxt@outlook.com>
2017-01-24 13:29:01 +0800

    TestDelete

--- SunBoom/src/main/java/dao/impl/GroupDAOImpl.java
+++ SunBoom/src/main/java/dao/impl/GroupDAOImpl.java
@@ -19,15 +19,8 @@ public class GroupDAOImpl extends BaseDAOImpl<Group> implements GroupDAO {
         g.setName("学生会");
         ArrayList<Group> list = new ArrayList<Group>();
         Group g2 = new Group(g);
- //       g2.setGroupID(3);
-        Group g3 = new Group(g);
-//        g3.setGroupID(4);
-        Group g4 = new Group(g);
-//        g4.setGroupID(6);
         list.add(g2);
-        list.add(g3);
-        list.add(g4);
-        impl.insertList(list);
+        g2.setGroupID(11);
+        impl.deleteList(list);
     }
-
 }
