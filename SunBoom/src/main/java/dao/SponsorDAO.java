65162c31f67ae37fc829e87c771697055220074f
wandererZoe <zoetxt@outlook.com>
2017-01-29 17:36:55 +0800

    SponsorDAO

---
+++ SunBoom/src/main/java/dao/SponsorDAO.java
@@ -0,0 +1,11 @@
+package dao;
+
+import dao.base.BaseDAO;
+import pojo.Sponsor;
+
+/**
+ * Created by zoetx on 2017/1/29.
+ */
+public interface SponsorDAO extends BaseDAO<Sponsor> {
+
+}
---
+++ SunBoom/src/main/java/dao/impl/SponsorDAOImpl.java
@@ -0,0 +1,13 @@
+package dao.impl;
+
+
+import dao.SponsorDAO;
+import dao.base.BaseDAOImpl;
+import pojo.Sponsor;
+
+/**
+ * Created by zoetx on 2017/1/29.
+ */
+public class SponsorDAOImpl extends BaseDAOImpl<Sponsor> implements SponsorDAO {
+
+}
