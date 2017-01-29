45facf901d264f7928dc678aaf4b68588042b94e
wandererZoe <zoetxt@outlook.com>
2017-01-29 18:26:34 +0800

    测试findByProperty

--- SunBoom/src/main/java/test/dao/impl/SchoolDAOImplTest.java
+++ SunBoom/src/main/java/test/dao/impl/SchoolDAOImplTest.java
@@ -5,6 +5,8 @@ import dao.impl.School2StudentDAOImpl;
 import org.junit.Test;
 import pojo.School2Student;
 
+import java.util.List;
+
 /**
  * Created by zoetx on 2017/1/29.
  */
@@ -14,7 +16,8 @@ public class SchoolDAOImplTest {
     @Test
     public void testSchool2StudentImpl() throws Exception {
         School2StudentDAO dao = new School2StudentDAOImpl();
-        School2Student s = dao.getById(0);
-        System.out.print(s.getIdentificationTime());
+//        School2Student s = dao.getById(0);
+        List<School2Student> list = dao.findByProperty("enrollmentYear",2016);
+        System.out.print(list.get(0).getIdentificationTime());
     }
 }
