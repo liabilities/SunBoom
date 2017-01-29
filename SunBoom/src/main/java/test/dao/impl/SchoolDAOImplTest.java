518ad7520cfb1dc09da5c50689be5f9243049693
wandererZoe <zoetxt@outlook.com>
2017-01-29 19:00:05 +0800

    测试findByProperty(map)

--- SunBoom/src/main/java/test/dao/impl/SchoolDAOImplTest.java
+++ SunBoom/src/main/java/test/dao/impl/SchoolDAOImplTest.java
@@ -5,7 +5,9 @@ import dao.impl.School2StudentDAOImpl;
 import org.junit.Test;
 import pojo.School2Student;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by zoetx on 2017/1/29.
@@ -17,7 +19,12 @@ public class SchoolDAOImplTest {
     public void testSchool2StudentImpl() throws Exception {
         School2StudentDAO dao = new School2StudentDAOImpl();
 //        School2Student s = dao.getById(0);
-        List<School2Student> list = dao.findByProperty("enrollmentYear",2016);
-        System.out.print(list.get(0).getIdentificationTime());
+//        List<School2Student> list = dao.findByProperty("enrollmentYear",2016);
+        Map<String,Object> map = new HashMap<String,Object>();
+        map.put("enrollmentYear",2016);
+        map.put("schoolID",0);
+        List<School2Student> list = dao.findByProperty(map);
+
+        System.out.print(list.size());
     }
 }
