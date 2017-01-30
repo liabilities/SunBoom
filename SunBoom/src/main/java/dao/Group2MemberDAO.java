4942d9cde7d489a5d899eca4766fd3ce9d0b1909
wandererZoe <zoetxt@outlook.com>
2017-01-30 23:14:30 +0800

    Group2Member

---
+++ SunBoom/src/main/java/dao/Group2MemberDAO.java
@@ -0,0 +1,11 @@
+package dao;
+
+import dao.base.BaseDAO;
+import pojo.Group2Member;
+
+/**
+ * Created by zoetx on 2017/1/29.
+ */
+public interface Group2MemberDAO extends BaseDAO<Group2Member> {
+
+}
--- SunBoom/src/main/java/test/dao/impl/GroupDAOImplTest.java
+++ SunBoom/src/main/java/test/dao/impl/GroupDAOImplTest.java
@@ -1,8 +1,11 @@
 package test.dao.impl;
 
+import dao.Group2MemberDAO;
+import dao.impl.Group2MemberDAOImpl;
 import dao.impl.GroupDAOImpl;
 import org.junit.Test;
 import pojo.Group;
+import pojo.Group2Member;
 
 /**
  * Created by zoetx on 2017/1/29.
@@ -15,5 +18,12 @@ public class GroupDAOImplTest {
         GroupDAOImpl impl = new GroupDAOImpl();
         Group g = impl.getById(1);
         System.out.print(g.getName());
+    }
+
+    @Test
+    public void testGroup2Member() throws Exception {
+        Group2MemberDAO impl = new Group2MemberDAOImpl();
+        Group2Member g = impl.getById(0);
+        System.out.print(g.getAddTime());
     }
 }
