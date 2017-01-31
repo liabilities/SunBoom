3eac4ae0816017a8eddba1ef75904ee45a47cccd
wandererZoe <zoetxt@outlook.com>
2017-01-31 12:59:30 +0800

    TempGroupDAO

---
+++ SunBoom/src/main/java/dao/TempGroupDAO.java
@@ -0,0 +1,11 @@
+package dao;
+
+import dao.base.BaseDAO;
+import pojo.TempGroup;
+
+/**
+ * Created by zoetx on 2017/1/29.
+ */
+public interface TempGroupDAO extends BaseDAO<TempGroup> {
+
+}
---
+++ SunBoom/src/main/java/dao/impl/TempGroupDAOImpl.java
@@ -0,0 +1,13 @@
+package dao.impl;
+
+
+import dao.TempGroupDAO;
+import dao.base.BaseDAOImpl;
+import pojo.TempGroup;
+
+/**
+ * Created by zoetx on 2017/1/29.
+ */
+public class TempGroupDAOImpl extends BaseDAOImpl<TempGroup> implements TempGroupDAO {
+
+}
