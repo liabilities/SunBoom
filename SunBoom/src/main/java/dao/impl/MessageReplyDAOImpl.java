641d83bec6eef9e5278998d8b86e46b8a0ba18e3
wandererZoe <zoetxt@outlook.com>
2017-02-02 15:52:01 +0800

    MessageReplyDAO

---
+++ SunBoom/src/main/java/dao/impl/MessageReplyDAOImpl.java
@@ -0,0 +1,12 @@
+package dao.impl;
+
+import dao.MessageReplyDAO;
+import dao.base.BaseDAOImpl;
+import pojo.MessageReply;
+
+/**
+ * Created by I Like Milk on 2017/1/11.
+ */
+public class MessageReplyDAOImpl extends BaseDAOImpl<MessageReply> implements MessageReplyDAO {
+
+}
--- SunBoom/src/main/java/test/dao/impl/MessageDAOImplTest.java
+++ SunBoom/src/main/java/test/dao/impl/MessageDAOImplTest.java
@@ -1,14 +1,25 @@
 package test.dao.impl;
 
 import dao.MessageDAO;
+import dao.MessageReplyDAO;
 import dao.impl.MessageDAOImpl;
+import dao.impl.MessageReplyDAOImpl;
 import org.junit.Test;
 import pojo.Message;
+import pojo.MessageReply;
 
 /**
  * Created by zoetx on 2017/1/29.
  */
 public class MessageDAOImplTest {
+
+    @Test
+    public void testMessageReplyDAOImpl() throws Exception {
+
+        MessageReplyDAO impl = new MessageReplyDAOImpl();
+        MessageReply g = impl.getById(0);
+        System.out.print(g.getContent());
+    }
 
 
     @Test
@@ -18,4 +29,5 @@ public class MessageDAOImplTest {
         Message g = impl.getById(0);
         System.out.print(g.getContent());
     }
+
 }
