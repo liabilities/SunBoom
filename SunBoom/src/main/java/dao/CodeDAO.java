9b4d8cc135e0ccb7bffe2e011c65276df2dbd312
wandererZoe <zoetxt@outlook.com>
2017-02-17 14:18:24 +0800

    CodeDAO

---
+++ SunBoom/src/main/java/dao/CodeDAO.java
@@ -0,0 +1,12 @@
+package dao;
+
+
+import dao.base.BaseDAO;
+import pojo.Code;
+
+/**
+ * Created by zoetx on 2017/1/29.
+ */
+public interface CodeDAO extends BaseDAO<Code> {
+
+}
---
+++ SunBoom/src/main/java/pojo/Code.java
@@ -0,0 +1,47 @@
+package pojo;
+
+import java.util.Date;
+
+/**
+ * Created by zoetx on 2017/1/12.
+ */
+public class Code {
+    private int rowID;
+    private int groupID;
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    private String code;
+    private Date ddl;
+
+    public int getRowID() {
+        return rowID;
+    }
+
+    public void setRowID(int rowID) {
+        this.rowID = rowID;
+    }
+
+    public int getGroupID() {
+        return groupID;
+    }
+
+    public void setGroupID(int groupID) {
+        this.groupID = groupID;
+    }
+
+
+    public Date getDdl() {
+        return ddl;
+    }
+
+    public void setDdl(Date ddl) {
+        this.ddl = ddl;
+    }
+}
